db6d7aaa341e539ad9f60a3f0ccb7e6cfe17726c
DarbyEntLLC <darbyentllc@gmail.com>
2025-05-30 18:09:57 -0400

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        'bounce': 'bounce 1s infinite',
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'spin': 'spin 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
