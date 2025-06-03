57f6b81949c5912a746f2148c94d50cb5c963835
DarbyEntLLC <darbyentllc@gmail.com>
2025-06-03 13:01:41 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backdropBlur: {
-        'xs': '2px',
-        'xl': '24px',
-        '2xl': '40px',
-        '3xl': '64px',
-      },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 }
