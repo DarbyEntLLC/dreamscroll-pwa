5198abe68b31ef154d2dad6f5265f59fc64c9dac
DarbyEntLLC <darbyentllc@gmail.com>
2025-06-16 13:01:59 -0400

    Update next.config.js
    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove the deprecated appDir configuration
+  // App directory is now the default in Next.js 14+
 }
 
 module.exports = nextConfig
